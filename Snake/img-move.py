62c89296c0398637405f09323db463c408744165
DAO <den.orlov@db.com>
2021-01-16 18:27:37 +0300

    simple img draw

--- Snake/img-move.py
+++ Snake/img-move.py
@@ -15,13 +15,24 @@ FPS = 7
 
 SCREEN_SIZE = WIDTH, HEIGHT = 800, 600
 
+start_x = 0
+speed = 15 # pixels per sec
+last_time_ms = 0
+
 def draw(screen):
+    global last_time_ms, start_x
+
+    time_delta_ms = clock.get_time()
+    dx = time_delta_ms * speed // 100
+    start_x = start_x + dx
+
     screen.fill(BLACK_COLOR)
 
-    screen.blit(bunny_image, (100, 100))
-    screen.blit(plant_img, (200, 200))
-    screen.blit(crystal_img, (300, 300))
-    screen.blit(rock_img, (-400, -400))
+    screen.blit(bunny_image, (100 + start_x, 100))
+    screen.blit(plant_img, (200 + start_x, 200))
+    screen.blit(crystal_img, (300 + start_x, 300))
+    screen.blit(rock_img, (400 + start_x, 400))
+
 
 pygame.init()
 pygame.display.set_caption("Картинка")
