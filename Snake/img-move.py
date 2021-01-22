2e85cd50b5d29d9cb149d6b69f44434b29b605f3
DAO <den.orlov@db.com>
2021-01-23 00:53:40 +0300

    simple img draw

--- Snake/img-move.py
+++ Snake/img-move.py
@@ -1,5 +1,3 @@
-from random import randint
-
 import pygame as pygame
 from pygame.font import Font
 
@@ -11,38 +9,15 @@ DARK_GREEN_COLOR = pygame.Color('forestgreen')
 GREEN_COLOR = pygame.Color('green')
 RED_COLOR = pygame.Color('red')
 
-FPS = 7
-
 SCREEN_SIZE = WIDTH, HEIGHT = 800, 600
 
-start_x = 0
-speed = 15 # pixels per sec
-last_time_ms = 0
-
-def draw(screen):
-    global last_time_ms, start_x
-
-    time_delta_ms = clock.get_time()
-    dx = time_delta_ms * speed // 100
-    start_x = start_x + dx
-
-    screen.fill(BLACK_COLOR)
-
-    screen.blit(bunny_image, (100 + start_x, 100))
-    screen.blit(plant_img, (200 + start_x, 200))
-    screen.blit(crystal_img, (300 + start_x, 300))
-    screen.blit(rock_img, (400 + start_x, 400))
-
-
 pygame.init()
-pygame.display.set_caption("Картинка")
+pygame.display.set_caption("Заставка")
 screen = pygame.display.set_mode(SCREEN_SIZE)
-font = Font(None, 24)
 
-bunny_image = pygame.image.load("bunny.png")
-plant_img = pygame.image.load("plant.png")
-crystal_img = pygame.image.load("crystal.png")
-rock_img = pygame.image.load("rock.png")
+intro_image = pygame.image.load("python_intro_img.jpg")
+start_x = -intro_image.get_width()
+speed = 25 # pixels per sec
 
 clock = pygame.time.Clock()
 
@@ -52,8 +27,15 @@ while is_running:
         if event.type == pygame.QUIT:
             is_running = False
 
-    draw(screen)
+    time_delta_ms = clock.get_time()
+    dx = time_delta_ms * speed // 100
+    start_x = start_x + dx
+
+    if start_x > 0:
+        start_x = 0
+
+    screen.blit(intro_image, (start_x, 0))
     pygame.display.flip()
-    clock.tick(FPS)
+    clock.tick(120)
 
 pygame.quit()
