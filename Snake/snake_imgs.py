e0a3dc064d23f8a1f44ec5c86417cf70a139ff8a
DAO <den.orlov@db.com>
2021-03-13 16:41:09 +0300

    snake with classes and pyanimation

--- Snake/snake_imgs.py
+++ Snake/snake_imgs.py
@@ -54,23 +54,3 @@ snake_mid_horizontal_img = pygame.transform.scale(snake_mid_horizontal_img, (TIL
 
 snake_mid_vertical_img = snake_img.subsurface((64 * 2, 64 * 1, 64, 64))
 snake_mid_vertical_img = pygame.transform.scale(snake_mid_vertical_img, (TILE, TILE))
-
-RABBIT_TILE = 32
-rabbit_spritesheet = pygame.image.load("rabbit.png")
-
-bunny_imgs = []  # a list for the bunny images
-# the spritesheet has bunny phases, 64 x 64 pixels
-for x in range(3):  # first line contains 8 pictures of bunny
-    for y in range(4): # 8 lines
-        subsurface = rabbit_spritesheet.subsurface(
-            0 + RABBIT_TILE * x,
-            6 + RABBIT_TILE * y,
-            RABBIT_TILE, RABBIT_TILE
-        )
-        bunny_imgs.append(subsurface)
-
-for n in range(len(bunny_imgs)):
-    bunny_imgs[n].set_colorkey(pygame.color.Color("#78C380"))
-    #bunny_imgs[n] = pygame.transform.scale(bunny_imgs[n], (TILE, TILE))
-
-bunny_animation_phases = [0, 4, 8,  5, 5, 5, 8, 0, 4, 6, 6, 6, 3, 3, 3, 5, 5, 5]
